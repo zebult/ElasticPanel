d5cc8c241f616fcc1eedcc403fccbabcfcff1f9c
zebult <suzuki_saichi@goodroid.co.jp>
2018-07-07 18:16:05 +0900

    Initial Commit

---
+++ ElasticPanel/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ElasticPanel
+//
+//  Created by 鈴木 才智 on 2018/07/07.
+//  Copyright © 2018年 鈴木 才智. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ElasticPanel/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ElasticPanel
+//
+//  Created by 鈴木 才智 on 2018/07/07.
+//  Copyright © 2018年 鈴木 才智. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+

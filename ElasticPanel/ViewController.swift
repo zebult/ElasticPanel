bd9679de05b75bac8b243f7ad4db552f394de160
zebult <suzuki_saichi@goodroid.co.jp>
2018-07-07 19:54:04 +0900

    add resize window
    diff --git a/ElasticPanel/ViewController.swift b/ElasticPanel/ViewController.swift
    index 07c3142..ad24f43 100644
    --- a/ElasticPanel/ViewController.swift
    +++ b/ElasticPanel/ViewController.swift
    @@ -25,35 +25,7 @@ class ViewController: NSViewController
             case right
         }
    
    -    override func viewDidLoad()
    -    {
    -        super.viewDidLoad()
    -
    -        NSEvent.addLocalMonitorForEvents(matching:.keyDown) {
    -            (event)->NSEvent? in
    -            self.keyDown(with : event)
    -
    -            return event
    -        }
    -    }
    -
    -    override func keyDown(with event : NSEvent)
    -    {
    -        let key       = String(describing : event.characters!);
    -        let direction = getDirection(key: key);
    -        let deltaSize = directionToSize(direction: direction);
    -
    -        resizeWindow(deltaSize: deltaSize);
    -    }
    -
    -    func resizeWindow(deltaSize : (CGFloat, CGFloat))
    -    {
    -        let frame = self.view.window!.frame;
    -
    -        self.view.window?.setFrame(NSRect(x: frame.minX - deltaSize.0, y: frame.minY, width: frame.width + deltaSize.0, height: frame.height + deltaSize.1), display: true)
    -    }
    -
    -    func getDirection(key : String)->Direction
    +    func getDirection(key: String)->Direction
         {
             switch key {
                 case "h":
    @@ -74,27 +46,46 @@ class ViewController: NSViewController
             }
         }
    
    -    func directionToSize(direction : Direction)->(CGFloat, CGFloat)
    +    override func viewDidLoad()
         {
    -        let size:(CGFloat, CGFloat)
    +        super.viewDidLoad()
    +
    +        NSEvent.addLocalMonitorForEvents(matching:.keyDown) {
    +            (event)->NSEvent? in
    +            self.keyDown(with : event)
    +
    +            return event
    +        }
    +    }
    
    -        switch direction {
    +    override func keyDown(with event : NSEvent)
    +    {
    +        let key       = String(describing : event.characters!);
    +        let direction = getDirection(key: key);
    +
    +        resizeWindow(direction: direction);
    +    }
    +
    +    func resizeWindow(direction : Direction)
    +    {
    +        let frame = self.view.window!.frame;
    +
    +        switch direction
    +        {
                 case Direction.left:
    -                size = (self.distanceX, 0);
    -            case Direction.down:
    +                self.view.window?.setFrame(NSRect(x: frame.minX - self.distanceX, y: frame.minY, width: frame.width + self.distanceX, height: frame.height), display: true)
    +            case Direction.down :
    
    -                size = (0, -self.distanceY);
    -            case Direction.up:
    +                self.view.window?.setFrame(NSRect(x: frame.minX, y: frame.minY - self.distanceY, width: frame.width, height: frame.height + self.distanceY), display: true)
    +            case Direction.up :
    
    -                size = (0, self.distanceY);
    -            case Direction.right:
    +                self.view.window?.setFrame(NSRect(x: frame.minX, y: frame.minY, width: frame.width, height: frame.height + self.distanceY), display: true)
    +            case Direction.right :
    
    -                size = (-self.distanceX, 0);
    -            default:
    +                self.view.window?.setFrame(NSRect(x: frame.minX, y: frame.minY, width: frame.width + self.distanceX, height: frame.height), display: true)
    +            default :
    
    -                size = (0, 0);
    +                return;
             }
    -
    -        return size;
         }
     }

--- ElasticPanel/ViewController.swift
+++ ElasticPanel/ViewController.swift
@@ -12,6 +12,40 @@ import Cocoa
 
 class ViewController: NSViewController
 {
+    private let distanceX: CGFloat = 10;
+
+    private let distanceY: CGFloat = 10;
+
+    enum Direction
+    {
+        case none
+        case left
+        case down
+        case up
+        case right
+    }
+
+    func getDirection(key: String)->Direction
+    {
+        switch key {
+            case "h":
+
+                return Direction.left;
+            case "j":
+
+                return Direction.down;
+            case "k":
+
+                return Direction.up;
+            case "l":
+
+                return Direction.right;
+            default:
+
+                return Direction.none;
+        }
+    }
+
     override func viewDidLoad()
     {
         super.viewDidLoad()
@@ -26,6 +60,32 @@ class ViewController: NSViewController
 
     override func keyDown(with event : NSEvent)
     {
-        print(String(describing: event.characters!))
+        let key       = String(describing : event.characters!);
+        let direction = getDirection(key: key);
+
+        resizeWindow(direction: direction);
+    }
+
+    func resizeWindow(direction : Direction)
+    {
+        let frame = self.view.window!.frame;
+
+        switch direction
+        {
+            case Direction.left:
+                self.view.window?.setFrame(NSRect(x: frame.minX - self.distanceX, y: frame.minY, width: frame.width + self.distanceX, height: frame.height), display: true)
+            case Direction.down :
+
+                self.view.window?.setFrame(NSRect(x: frame.minX, y: frame.minY - self.distanceY, width: frame.width, height: frame.height + self.distanceY), display: true)
+            case Direction.up :
+
+                self.view.window?.setFrame(NSRect(x: frame.minX, y: frame.minY, width: frame.width, height: frame.height + self.distanceY), display: true)
+            case Direction.right :
+
+                self.view.window?.setFrame(NSRect(x: frame.minX, y: frame.minY, width: frame.width + self.distanceX, height: frame.height), display: true)
+            default :
+
+                return;
+        }
     }
 }

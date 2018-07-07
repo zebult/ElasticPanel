32e48dc70016a32eab97fbb8aece1eb17ff84ec4
zebult <suzuki_saichi@goodroid.co.jp>
2018-07-07 18:36:59 +0900

    update uncs

--- ElasticPanel/ViewController.swift
+++ ElasticPanel/ViewController.swift
@@ -1,27 +1,30 @@
-//
-//  ViewController.swift
-//  ElasticPanel
-//
-//  Created by 鈴木 才智 on 2018/07/07.
-//  Copyright © 2018年 鈴木 才智. All rights reserved.
-//
+/*
+ *
+ *  ViewController.swift
+ *  ElasticPanel
+ *
+ *  Created by 鈴木 才智 on 2018/07/07.
+ *  Copyright © 2018年 鈴木 才智. All rights reserved.
+ *
+ */
 
 import Cocoa
 
-class ViewController: NSViewController {
-
-    override func viewDidLoad() {
+class ViewController: NSViewController
+{
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        /* Do any additional setup after loading the view. */
     }
 
-    override var representedObject: Any? {
-        didSet {
-        // Update the view, if already loaded.
+    override var representedObject: Any?
+    {
+        didSet
+        {
+            /* Update the view, if already loaded. */
         }
     }
-
-
 }
 

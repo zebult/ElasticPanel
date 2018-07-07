43c07c92bb36a9f60a6576fb64d84b3fddc73808
zebult <suzuki_saichi@goodroid.co.jp>
2018-07-07 22:02:11 +0900

    add config key

--- ElasticPanel/ViewController.swift
+++ ElasticPanel/ViewController.swift
@@ -12,9 +12,9 @@ import Cocoa
 
 class ViewController: NSViewController
 {
-    private let distanceX: CGFloat = 10;
+    private var deltaX: CGFloat = 10;
 
-    private let distanceY: CGFloat = 10;
+    private var deltaY: CGFloat = 10;
 
     private let initHeight: CGFloat = 100;
 
@@ -28,6 +28,7 @@ class ViewController: NSViewController
         case up
         case right
         case reset
+        case config
     }
 
     func getKeyType(key: String)->KeyType
@@ -48,10 +49,37 @@ class ViewController: NSViewController
             case "r":
 
                 return KeyType.reset;
+            case "c":
+
+                return KeyType.config;
             default:
 
                 return KeyType.none;
         }
+    }
+
+    @IBOutlet weak var xLabel: NSTextField!
+
+    @IBOutlet weak var yLabel: NSTextField!
+
+    @IBOutlet weak var xTextField: NSTextField!
+
+    @IBOutlet weak var yTextField: NSTextField!
+
+    @IBAction func onChangeX(_ sender : NSTextField)
+    {
+        let title:String? = sender.cell?.title;
+        UserDefaults.standard.setValue(title!, forKey: "delta_x");
+
+        self.deltaX = strToCGFloat(str: title!);
+    }
+
+    @IBAction func onChangeY(_ sender : NSTextField)
+    {
+        let title : String? = sender.cell?.title;
+        UserDefaults.standard.setValue(title!, forKey: "delta_y");
+
+        self.deltaY = strToCGFloat(str: title!);
     }
 
     override func viewDidLoad()
@@ -64,6 +92,30 @@ class ViewController: NSViewController
 
             return event
         }
+
+        if UserDefaults.standard.object(forKey : "delta_x") == nil
+        {
+            UserDefaults.standard.setValue(self.deltaX, forKey: "delta_x");
+        }
+
+        if UserDefaults.standard.object(forKey: "delta_y") == nil
+        {
+            UserDefaults.standard.setValue(self.deltaY, forKey: "delta_y");
+        }
+
+        if UserDefaults.standard.object(forKey: "hide_config") == nil
+        {
+            UserDefaults.standard.setValue(false, forKey: "hide_config");
+        }
+
+        let deltaX : String = UserDefaults.standard.string(forKey: "delta_x")!;
+        let deltaY : String = UserDefaults.standard.string(forKey: "delta_y")!;
+        self.xTextField.cell?.title = deltaX;
+        self.yTextField.cell?.title = deltaY;
+        self.deltaX                   = strToCGFloat(str: deltaX);
+        self.deltaY                   = strToCGFloat(str: deltaY);
+
+        updateConfig();
     }
 
     override func viewWillAppear()
@@ -93,22 +145,45 @@ class ViewController: NSViewController
         switch direction
         {
             case KeyType.left:
-                self.view.window?.setFrame(NSRect(x: frame.minX - self.distanceX, y: frame.minY, width: frame.width + self.distanceX, height: frame.height), display: true)
+                self.view.window?.setFrame(NSRect(x: frame.minX - self.deltaX, y: frame.minY, width: frame.width + self.deltaX, height: frame.height), display: true)
             case KeyType.down :
 
-                self.view.window?.setFrame(NSRect(x: frame.minX, y: frame.minY - self.distanceY, width: frame.width, height: frame.height + self.distanceY), display: true)
+                self.view.window?.setFrame(NSRect(x: frame.minX, y: frame.minY - self.deltaY, width: frame.width, height: frame.height + self.deltaY), display: true)
             case KeyType.up :
 
-                self.view.window?.setFrame(NSRect(x: frame.minX, y: frame.minY, width: frame.width, height: frame.height + self.distanceY), display: true)
+                self.view.window?.setFrame(NSRect(x: frame.minX, y: frame.minY, width: frame.width, height: frame.height + self.deltaY), display: true)
             case KeyType.right :
 
-                self.view.window?.setFrame(NSRect(x: frame.minX, y: frame.minY, width: frame.width + self.distanceX, height: frame.height), display: true)
+                self.view.window?.setFrame(NSRect(x: frame.minX, y: frame.minY, width: frame.width + self.deltaX, height: frame.height), display: true)
             case KeyType.reset :
 
                 self.view.window?.setFrame(NSRect(x: 0, y: self.screenSize.height - self.initHeight, width: self.screenSize.width, height: self.initHeight), display: true)
-            default :
+            case KeyType.config :
+                let hideConfig = UserDefaults.standard.bool(forKey: "hide_config");
+                UserDefaults.standard.setValue(!hideConfig, forKey: "hide_config");
+                updateConfig();
+            default:
 
                 return;
         }
+    }
+
+    func updateConfig()
+    {
+        let hideConfig = UserDefaults.standard.bool(forKey:"hide_config");
+        self.xLabel.isHidden     = hideConfig;
+        self.yLabel.isHidden     = hideConfig;
+        self.xTextField.isHidden = hideConfig;
+        self.yTextField.isHidden = hideConfig;
+    }
+
+    func strToCGFloat(str : String)->CGFloat
+    {
+        if let number = NumberFormatter().number(from: str)
+        {
+            return CGFloat(truncating: number);
+        }
+
+        return 0;
     }
 }
